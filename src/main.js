b3a0088c507b96e7393b84b19517d6cb9a4c9854
givanyo <giovanniviana22@gmail.com>
2025-12-09 22:51:59 -0300

    primeira animacao

--- src/main.js
+++ src/main.js
@@ -3,4 +3,45 @@ import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+const contentSections = gsap.utils.toArray('.content');
+
+gsap.set('.left', {
+    x: '-100vw'
+})
+
+gsap.set('.right', {
+    x: '100vw'
+})
+contentSections.forEach((section) => {
+    const leftChildren = section.querySelectorAll('.left');
+    const rightChildren = section.querySelectorAll('.right');
+
+    gsap.to(leftChildren, {
+        x: 0,
+        ease: 'power1.inOut',
+        scrollTrigger: {
+            trigger: section,
+            scrub: true,
+            markers: true,
+            start: 'top 120%',
+            end: 'top 30%',
+
+        }
+    })
+
+    gsap.to(rightChildren, {
+        x: 0,
+        ease: 'power1.inOut',
+        scrollTrigger: {
+            trigger: section,
+            scrub: true,
+            markers: true,
+            start: 'top 100%',
+            end: 'top 30%'
+        }
+    })
+})
+
+
+
 
